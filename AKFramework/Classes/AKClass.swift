fe83403e39ba42d1228a931ea986eba3615cef1c
akirichek <askirichek@gmail.com>
2019-01-15 16:31:49 +0200

    Second commit

---
+++ AKFramework/Classes/AKClass.swift
@@ -0,0 +1,15 @@
+//
+//  AKClass.swift
+//  AKFramework
+//
+//  Created by Artem Kirichek on 1/15/19.
+//
+
+import Foundation
+
+public class AKClass {
+    
+    public static func sayHello() {
+        print("AKClass: Hello!")
+    }
+}
--- Example/AKFramework/ViewController.swift
+++ Example/AKFramework/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import AKFramework
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        AKClass.sayHello()
     }
 
     override func didReceiveMemoryWarning() {
